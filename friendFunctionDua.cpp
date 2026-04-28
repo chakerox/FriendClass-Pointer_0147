ec1912432f4fb1eb43be0bff3adf5c3595f4bbc7
chakerox <chakerox@gmail.com>
2026-04-28 19:51:37 +0700

    memperbaiki bug yang kurang

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -1,24 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class pelajar;
-class manusia{
-    public:
+class manusia
+{
+public:
     void showNilaiPelajar(pelajar &x);
 };
 
-class pelajar{
-    private:
+class pelajar
+{
+private:
     int nilai;
-    public:
-    friend void manusia :: showNilaiPelajar(pelajar &x);
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
 };
 
-void manusia :: showNilaiPelajar(pelajar &x){
+void manusia ::showNilaiPelajar(pelajar &x)
+{
     cout << x.nilai;
-};
-
-int main(){
+}
+int main()
+{
     manusia budi;
     pelajar pbudi;
     budi.showNilaiPelajar(pbudi);
